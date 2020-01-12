205acc857f1cb80476985603ce21a18670660025
Luke Smetham <luke@arkitek.co.uk>
2020-01-12 17:31:05 +0100

    prettier

--- dist/index.js
+++ dist/index.js
@@ -3,33 +3,13 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.default = void 0;
+Object.defineProperty(exports, "useResize", {
+  enumerable: true,
+  get: function () {
+    return _useResize.default;
+  }
+});
 
-var _fetch = _interopRequireDefault(require("./utils/fetch"));
+var _useResize = _interopRequireDefault(require("./useResize"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-class Stats {
-  constructor() {
-    this.fetch = new _fetch.default();
-  }
-
-  async pkg({
-    start,
-    end,
-    pkg
-  }) {
-    try {
-      return await this.fetch.get({
-        start,
-        end,
-        pkg
-      });
-    } catch (error) {
-      return new Error(error);
-    }
-  }
-
-}
-
-exports.default = Stats;
