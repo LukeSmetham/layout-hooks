418913a486c7728891892a8c3050708925ff2ee2
Luke Smetham <luke@arkitek.co.uk>
2020-01-12 19:32:20 +0100

    fix for useLayout

--- src/useLayout.js
+++ src/useLayout.js
@@ -5,16 +5,16 @@ export default ref => {
 	const [layout, setLayout] = useState(null);
 
 	useEffect(() => {
+		const observer = new ResizeObserver(entries => {
+			const [entry] = entries;
+			setLayout(entry.contentRect);
+		});
+
 		if (ref.current) {
-			const observer = new ResizeObserver(entries => {
-				const [entry] = entries;
-				setLayout(entry.contentRect);
-			});
-
 			observer.observe(ref.current);
-
-			return observer.disconnect();
 		}
+
+		return observer.disconnect();
 	}, [ref.current]);
 
 	return layout;
