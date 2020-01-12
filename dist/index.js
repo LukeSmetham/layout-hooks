09dd8b96ff945d448a433acc6b19dc903ab9438c
Luke Smetham <luke@arkitek.co.uk>
2020-01-12 19:00:31 +0100

    prettier

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,7 @@
 "use strict";
 
+function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
@@ -15,6 +17,18 @@ Object.defineProperty(exports, "useMedia", {
     return _useMedia["default"];
   }
 });
+Object.defineProperty(exports, "BreakpointProvider", {
+  enumerable: true,
+  get: function get() {
+    return _useMedia.BreakpointProvider;
+  }
+});
+Object.defineProperty(exports, "BreakpointContext", {
+  enumerable: true,
+  get: function get() {
+    return _useMedia.BreakpointContext;
+  }
+});
 Object.defineProperty(exports, "useStyledMedia", {
   enumerable: true,
   get: function get() {
@@ -24,8 +38,12 @@ Object.defineProperty(exports, "useStyledMedia", {
 
 var _useLayout = _interopRequireDefault(require("./useLayout"));
 
-var _useMedia = _interopRequireDefault(require("./useMedia"));
+var _useMedia = _interopRequireWildcard(require("./useMedia"));
 
 var _useStyledMedia = _interopRequireDefault(require("./useStyledMedia"));
+
+function _getRequireWildcardCache() { if (typeof WeakMap !== "function") return null; var cache = new WeakMap(); _getRequireWildcardCache = function _getRequireWildcardCache() { return cache; }; return cache; }
+
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } if (obj === null || _typeof(obj) !== "object" && typeof obj !== "function") { return { "default": obj }; } var cache = _getRequireWildcardCache(); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj["default"] = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
