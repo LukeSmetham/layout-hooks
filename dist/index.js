296123140aba5e62481488f0a367b68ad53e0369
Luke Smetham <luke@arkitek.co.uk>
2020-01-12 18:16:31 +0100

    prettier

--- dist/index.js
+++ dist/index.js
@@ -1,17 +1,23 @@
-'use strict';
+"use strict";
 
-Object.defineProperty(exports, '__esModule', {
-	value: true,
+Object.defineProperty(exports, "__esModule", {
+  value: true
 });
-Object.defineProperty(exports, 'useResize', {
-	enumerable: true,
-	get: function() {
-		return _useResize.default;
-	},
+Object.defineProperty(exports, "useResize", {
+  enumerable: true,
+  get: function () {
+    return _useResize.default;
+  }
+});
+Object.defineProperty(exports, "useStyledMedia", {
+  enumerable: true,
+  get: function () {
+    return _useStyledMedia.default;
+  }
 });
 
-var _useResize = _interopRequireDefault(require('./useResize'));
+var _useResize = _interopRequireDefault(require("./useResize"));
 
-function _interopRequireDefault(obj) {
-	return obj && obj.__esModule ? obj : { default: obj };
-}
+var _useStyledMedia = _interopRequireDefault(require("./useStyledMedia"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
