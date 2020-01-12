47d8223145181226387a0e5bf207b8ab2f39e803
Luke Smetham <luke@arkitek.co.uk>
2020-01-12 19:12:46 +0100

    useLayout tweaks

---
+++ src/useLayout.js
@@ -0,0 +1,19 @@
+import React, { useEffect, useState } from 'react'; // eslint-disable-line no-unused-vars
+import ResizeObserver from 'resize-observer-polyfill';
+
+export default ref => {
+	const [layout, setLayout] = useState(null);
+
+	useEffect(() => {
+		const observer = new ResizeObserver((entries, observer) => {
+			const [entry] = entries;
+			setLayout(entry.contentRect);
+		});
+
+		observer.observe(ref.current);
+
+		return observer.disconnect();
+	}, [ref]);
+
+	return layout;
+};
