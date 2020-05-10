3ffec321afb506843c90dea2bbb64ab8343d59a2
Luke Smetham <luke@arkitek.co.uk>
2020-05-10 09:01:35 +0200

    added useRAF

---
+++ src/useRAF.js
@@ -0,0 +1,18 @@
+import { useCallback, useEffect, useRef } from 'react';
+
+export default callback => {
+    const raf = useRef();
+
+    const handleRaf = useCallback(
+        time => {
+            callback(time);
+            raf.current = window.requestAnimationFrame(handleRaf);
+        },
+        [callback]
+    );
+
+    useEffect(() => {
+        raf.current = window.requestAnimationFrame(handleRaf);
+        return () => window.cancelAnimationFrame(handleRaf);
+    }, []);
+};
